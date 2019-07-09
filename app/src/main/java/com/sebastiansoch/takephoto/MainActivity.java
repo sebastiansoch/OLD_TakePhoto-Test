d8f7df9388b655c044f896b7dd763e8cc2eeedc9
Sebastian Soch <sebastian.soch@gmail.com>
2019-07-09 21:08:29 +0200

    Save full-size photo

--- app/src/main/java/com/sebastiansoch/takephoto/MainActivity.java
+++ app/src/main/java/com/sebastiansoch/takephoto/MainActivity.java
@@ -4,22 +4,32 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private final int REQ_CAMERA_PERMISSION = 10;
     private final int REQ_TAKE_PHOTO_ACTIV = 1;
+    private String currentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +50,30 @@ public class MainActivity extends AppCompatActivity {
     private void takePhotoIntent() {
         Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePhotoIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePhotoIntent, REQ_TAKE_PHOTO_ACTIV);
+
+            File photoFile = null;
+
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.sebastiansoch.takephoto.fileprovider", photoFile);
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePhotoIntent, REQ_TAKE_PHOTO_ACTIV);
+            }
         }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storeDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storeDir);
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     @Override
@@ -49,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQ_TAKE_PHOTO_ACTIV && resultCode == RESULT_OK) {
+            //Czemu nie ma extras jak Intencja dostaje takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
             Bitmap photoIcon = (Bitmap) data.getExtras().get("data");
             ImageView photoIconView = findViewById(R.id.smallPhotoIconImageView);
             photoIconView.setImageBitmap(photoIcon);
